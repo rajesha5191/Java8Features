fa660b07c1ad462013e685fdb23b21c78c81e8a2
Rajesh <rajesha5191@gmail.com>
2024-09-04 19:46:46 +0530

    Second commit

--- practice3.java
+++ practice3.java
@@ -3,5 +3,7 @@ package Java8Features;
 public class practice3 {
     public static void main(String[] args) {
         System.out.println("class just add git repo");
+        System.out.println("Modifi the file");
+        System.out.println("add to git");
     }
 }
